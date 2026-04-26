13634f4b724712cf60cfd5e6168809b0110d3b39
Cyprien Ricque <cyprien.ricque@epitech.eu>
2026-04-26 22:08:06 +0200

    Initial Commit

---
+++ dictate/dictateApp.swift
@@ -0,0 +1,17 @@
+//
+//  dictateApp.swift
+//  dictate
+//
+//  Created by Cyprien Ricque on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct dictateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dictateTests/dictateTests.swift
@@ -0,0 +1,19 @@
+//
+//  dictateTests.swift
+//  dictateTests
+//
+//  Created by Cyprien Ricque on 4/26/26.
+//
+
+import Testing
+@testable import dictate
+
+struct dictateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
